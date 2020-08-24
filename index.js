5dd4f46af9d43462b96119c2d23db109f24c9f3b
Macarealeportelli <macarenarealeportelli@gmail.com>
2020-08-24 20:08:00 -0300

    practica con DOM

---
+++ index.js
@@ -0,0 +1,8 @@
+const todosLosParrafos = document.getElementsByTagName ("p")
+console.log (todosLosParrafos)
+
+const todosLosVerdes = document.getElementsByClassName ("verde")
+console.log (todosLosVerdes)
+
+const todosLosBordesRojos = document.getElementById ("borde-rojo")
+console.log (todosLosBordesRojos)
