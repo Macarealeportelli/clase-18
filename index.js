261d8dc748b73b923be5163d5fe505fa8f975561
Macarealeportelli <macarenarealeportelli@gmail.com>
2020-08-24 21:21:32 -0300

    practicando con DOM

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-const todosLosParrafos = document.getElementsByTagName ("p")
-console.log (todosLosParrafos)
+// const todosLosParrafos = document.getElementsByTagName ("p")
+// console.log (todosLosParrafos)
 
-const todosLosVerdes = document.getElementsByClassName ("verde")
-console.log (todosLosVerdes)
+// const todosLosVerdes = document.getElementsByClassName ("verde")
+// console.log (todosLosVerdes)
 
-const todosLosBordesRojos = document.getElementById ("borde-rojo")
-console.log (todosLosBordesRojos)
+// const todosLosBordesRojos = document.getElementById ("borde-rojo")
+// console.log (todosLosBordesRojos)
+
+const elementoClaseVerde = document.querySelector(".verde")
+const elementoClaseRojo = document.querySelector(".rojo")
+const elementoClaseAmarillo = document.querySelector(".amarillo")
+const elementoClaseAzul = document.querySelector(".azul")
+const elementoClaseRosa = document.querySelector(".rosa")
+
+console.log (elementoClaseVerde.classList.add("negro"))
+console.log (elementoClaseRojo.classList.add("rosa"))
+console.log (elementoClaseRosa.classList.toggle("negro"))
+console.log (elementoClaseAmarillo.classList.contains("rojo"))
